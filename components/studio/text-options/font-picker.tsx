7642a0975bd09a9cb3a92c260b52b2b9aff4915c
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-04 18:11:17 -0500

    🚀 code-splitting dynamic font-picker for build

--- components/studio/text-options/font-picker.tsx
+++ components/studio/text-options/font-picker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { PureComponent } from 'react'
 import { Check, ChevronsUpDown, Loader2 } from 'lucide-react'
 import { Button } from '@/components/ui/button'
--- components/studio/text-options/index.tsx
+++ components/studio/text-options/index.tsx
@@ -1,22 +1,35 @@
+"use client";
+
+import dynamic from 'next/dynamic'
 import { Type } from 'lucide-react'
-import AddTextLayer from './add-text-layer'
-import FontSettings from './font-settings'
 import { useSelectedLayers } from '@/store/use-image-options'
 
-export default function TextOptions() {
-  const { selectedText } = useSelectedLayers()
+const AddTextLayer = dynamic(() => import('./add-text-layer'), {
+    ssr: false
+})
 
-  return (
-    <div className="flex w-full flex-col gap-8">
-      <AddTextLayer />
+const FontSettings = dynamic(() => import('./font-settings'), {
+    ssr: false
+})
 
-      <h3 className="flex items-center gap-2 text-xs font-medium uppercase text-dark/70">
-        <Type size={20} />
-        <span>Appearance</span>
-      </h3>
-      <div className={`${selectedText ? '' : 'opacity-40 pointer-events-none'}`}>
-        <FontSettings />
-      </div>
-    </div>
-  )
+const TextOptions = () => {
+    const { selectedText } = useSelectedLayers()
+
+    return (
+        <div className="flex w-full flex-col gap-8">
+            <AddTextLayer />
+
+            <h3 className="flex items-center gap-2 text-xs font-medium uppercase text-dark/70">
+                <Type size={20} />
+                <span>Appearance</span>
+            </h3>
+            <div className={`${selectedText ? '' : 'opacity-40 pointer-events-none'}`}>
+                <FontSettings />
+            </div>
+        </div>
+    )
 }
+
+export default dynamic(() => Promise.resolve(TextOptions), {
+    ssr: false
+})
