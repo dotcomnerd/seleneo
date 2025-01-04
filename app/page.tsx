91d82bdee3cc5902d3113ebb038ba4fbe70ca32f
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2025-01-04 19:35:00 +0000

    🫨 build fixes? (document still undefined, ugh.)

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 
 import dynamic from 'next/dynamic';
-import { Header as OldNavBar } from "@/components/header"
-import { Hero as OldHero } from "@/components/hero"
 import { Hero as NewHero } from "@/components/marketing/hero/main"
-import { Features as OldFeatures } from "@/components/features"
-import { Features as NewFeatures } from "@/components/marketing/features/main"
-import { CallToAction } from "@/components/marketing/call-to-action"
-import { FAQ } from "@/components/marketing/faq"
-import { Footer as OldFooter } from "@/components/footer"
 import { Footer as NewFooter } from "@/components/marketing/footer"
 import { Navbar as NewNavbar } from "@/components/marketing/nav"
-import { HowItWorks } from "@/components/marketing/how-it-works"
-import { ProductShowcase } from "@/components/marketing/product"
-import { FloatingElements } from "@/components/marketing/floating-icons"
+// import { Features as OldFeatures } from "@/components/features"
+// import { Features as NewFeatures } from "@/components/marketing/features/main"
+// import { CallToAction } from "@/components/marketing/call-to-action"
+// import { FAQ } from "@/components/marketing/faq"
+// import { Header as OldNavBar } from "@/components/header"
+// import { Hero as OldHero } from "@/components/hero"
+// import { Footer as OldFooter } from "@/components/footer"
+// import { HowItWorks } from "@/components/marketing/how-it-works"
+// import { ProductShowcase } from "@/components/marketing/product"
+// import { FloatingElements } from "@/components/marketing/floating-icons"
 
 const LazyFloatingElements = dynamic(() => import('@/components/marketing/floating-icons').then(mod => mod.FloatingElements), {
   loading: () => <div>Loading...</div>,
