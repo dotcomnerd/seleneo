aead46d2ef18e4326fc66fc4c9830c99cc27c997
dotcomnerd <70242273+dotcomnerd@users.noreply.github.com>
2025-01-04 11:04:49 +0000

    💨 add blob keyframes and animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -90,10 +90,46 @@ module.exports = {
                     from: { height: "var(--radix-accordion-content-height)" },
                     to: { height: "0" },
                 },
+                blob: {
+                    '0%': {
+                      transform: 'translate(0px, 0px) scale(1)',
+                    },
+                    '33%': {
+                      transform: 'translate(30px, -50px) scale(1.1)',
+                    },
+                    '66%': {
+                      transform: 'translate(-20px, 20px) scale(0.9)',
+                    },
+                    '100%': {
+                      transform: 'translate(0px, 0px) scale(1)',
+                    },
+                  },
+                  float: {
+                    '0%, 100%': {
+                      transform: 'translateY(0)',
+                    },
+                    '50%': {
+                      transform: 'translateY(-20px)',
+                    },
+                  },
+                "shiny-text": {
+                    "0%, 90%, 100%": {
+                        "background-position": "calc(-100% - var(--shiny-width)) 0",
+                    },
+                    "30%, 60%": {
+                        "background-position": "calc(100% + var(--shiny-width)) 0",
+                    },
+                },
             },
             animation: {
                 "accordion-down": "accordion-down 0.2s ease-out",
                 "accordion-up": "accordion-up 0.2s ease-out",
+                blob: 'blob 7s infinite',
+                'float-slow': 'float 8s ease-in-out infinite',
+                'float': 'float 6s ease-in-out infinite',
+                'float-fast': 'float 4s ease-in-out infinite',
+                'float-delayed': 'float 6s ease-in-out infinite 2s',
+                "shiny-text": "shiny-text 8s infinite",
             },
         },
     },
