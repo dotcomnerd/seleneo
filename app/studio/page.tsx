0c8bf8293c6b05294b93797aa21d861e8e9a079c
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-05 07:17:45 -0500

    🗣️ studio SEO metadata & better spinner

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -1,10 +1,60 @@
 import { Navbar } from "@/components/navbar";
 import { Sidebar } from "@/components/sidebar";
+import Spinner from "@/components/spinner/spinner";
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
+
+export const metadata: Metadata = {
+    generator: 'Next.js Baby!',
+    applicationName: 'Seleneo',
+    title: 'Studio - Seleneo',
+    description: 'The Free Design Tooling Built for Humans',
+    robots: 'index, follow',
+    referrer: 'origin-when-cross-origin',
+    keywords: ['design', 'tooling', 'open-source', 'digital assets', 'social media', 'images',
+        'thumbnails', 'screenshots', 'free design tooling', 'seleneo', 'seleneo design tooling',
+        'graphics', 'editing', 'creative', 'visual', 'marketing', 'branding', 'digital tools',
+        'image editor', 'social media tools', 'content creation', 'web design', 'UI/UX', 'thumbnail generator',
+        'design software', 'media creation', 'asset management', 'social graphics', 'visual content',
+        'design platform', 'digital marketing', 'image optimization', 'design resources', 'content tools',
+        'digital design', 'online editor', 'web tools', 'creative suite', 'design automation',
+        'social assets', 'media toolkit'],
+    authors: [{ name: 'Nyumat', url: 'https://github.com/nyumat' }],
+    creator: 'Nyumat',
+    publisher: 'Nyumat',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    metadataBase: new URL('https://freedesign.fyi'),
+    alternates: {
+        canonical: '/',
+    },
+    openGraph: {
+        title: 'Studio - Seleneo',
+        description: 'The Free Design Tooling Built for Humans',
+        url: 'https://freedesign.fyi',
+        siteName: 'Seleneo',
+        images: [
+            {
+                url: 'http://freedesign.fyi/api/og?title=Studio',
+                width: 1800,
+                height: 1600,
+                alt: 'Seleneo Studio',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+}
 
 const Canvas = dynamic(() => import('@/components/studio/studio-main'), {
     ssr: false,
-    loading: () => <div className="flex-1 flex items-center justify-center">Loading...</div>
+    loading: () =>
+        <div className="flex-1 flex items-center justify-center h-full w-full">
+            <Spinner />
+        </div>
 });
 
 export default function StudioPage() {
