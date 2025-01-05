f62a3f67480831793f0d7d0f0ec9686e4a5d97f8
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-05 04:11:59 -0500

    🚀 metadata base SEO + prefetching studio

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,52 @@ import { Providers } from '@/providers/index'
 import "@/styles/globals.css"
 import { GeistMono } from 'geist/font/mono'
 import { GeistSans } from 'geist/font/sans'
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    generator: 'Next.js Baby!',
+    applicationName: 'Seleneo',
+    title: 'The Free Design Tooling Built for Humans',
+    description: 'Create open graph images, thumbnails, and social media posts with Seleneo—an open-source platform for crafting beautiful digital assets.',
+    robots: 'index, follow',
+    referrer: 'origin-when-cross-origin',
+    keywords: ['design', 'tooling', 'open-source', 'digital assets', 'social media', 'images',
+        'thumbnails', 'screenshots', 'free design tooling', 'seleneo', 'seleneo design tooling',
+        'graphics', 'editing', 'creative', 'visual', 'marketing', 'branding', 'digital tools',
+        'image editor', 'social media tools', 'content creation', 'web design', 'UI/UX', 'thumbnail generator',
+        'design software', 'media creation', 'asset management', 'social graphics', 'visual content',
+        'design platform', 'digital marketing', 'image optimization', 'design resources', 'content tools',
+        'digital design', 'online editor', 'web tools', 'creative suite', 'design automation',
+        'social assets', 'media toolkit'],
+    authors: [{ name: 'Nyumat', url: 'https://github.com/nyumat' }],
+    creator: 'Nyumat',
+    publisher: 'Nyumat',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    metadataBase: new URL('https://freedesign.fyi'),
+    alternates: {
+        canonical: '/',
+    },
+    openGraph: {
+        title: 'The Free Design Tooling Built for Humans',
+        description: 'Create open graph images, thumbnails, and social media posts with Seleneo—an open-source platform for crafting beautiful digital assets.',
+        url: 'https://freedesign.fyi',
+        siteName: 'Seleneo',
+        images: [
+            {
+                url: 'http://freedesign.fyi/api/og?title=Seleneo',
+                width: 1800,
+                height: 1600,
+                alt: 'Seleneo Open Graph Image',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+}
 
 export default function RootLayout({
     children,
--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,35 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-import { Hero as NewHero } from "@/components/marketing/hero/main"
-import { Footer as NewFooter } from "@/components/marketing/footer"
-import { Navbar as NewNavbar } from "@/components/marketing/nav"
-import { Features as OldFeatures } from "@/components/features"
-import { Features as NewFeatures } from "@/components/marketing/features/main"
-import { CallToAction } from "@/components/marketing/call-to-action"
-import { FAQ } from "@/components/marketing/faq"
-import { Header as OldNavBar } from "@/components/header"
-import { Hero as OldHero } from "@/components/hero"
-import { Footer as OldFooter } from "@/components/footer"
-import { HowItWorks } from "@/components/marketing/how-it-works"
-import { ProductShowcase } from "@/components/marketing/product"
-import { FloatingElements } from "@/components/marketing/floating-icons"
-
+import { CallToAction } from "@/components/marketing/call-to-action";
+import { FAQ } from "@/components/marketing/faq";
+import { Features as NewFeatures } from "@/components/marketing/features/main";
+import { FloatingElements } from "@/components/marketing/floating-icons";
+import { Footer as NewFooter } from "@/components/marketing/footer";
+import { Hero as NewHero } from "@/components/marketing/hero/main";
+import { HowItWorks } from "@/components/marketing/how-it-works";
+import { Navbar as NewNavbar } from "@/components/marketing/nav";
+import { ProductShowcase } from "@/components/marketing/product";
+import { useRouter } from "next/navigation";
+import { useEffect } from 'react';
 
 export default function LandingPage() {
-  return (
-    <>
-      <NewNavbar />
-      <main>
-        <NewHero />
-        <FloatingElements />
-        <NewFeatures />
-        <ProductShowcase />
-        <HowItWorks />
-        <CallToAction />
-        <FAQ />
-      </main>
-      <NewFooter />
-    </>
-  )
+    const router = useRouter();
+    useEffect(() => {
+        router.prefetch("/studio");
+    }, [router]);
+    return (
+        <>
+            <NewNavbar />
+            <main>
+                <NewHero />
+                <FloatingElements />
+                <NewFeatures />
+                <ProductShowcase />
+                <HowItWorks />
+                <CallToAction />
+                <FAQ />
+            </main>
+            <NewFooter />
+        </>
+    )
 }
