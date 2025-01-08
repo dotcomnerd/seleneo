12ff55ca70dae0c9d826e1804fcf79160c044100
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-07 21:11:43 -0500

    🥲 fix screen-size warning hook, crying in da club

--- hooks/canvas-area-hooks/use-screen-size-warning-toast.ts
+++ hooks/canvas-area-hooks/use-screen-size-warning-toast.ts
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react'
-import { toast } from '../use-toast'
-
-/**
- * This hook shows a warning toast if the screen size is less than 768px.
- */
+import { useRouter } from 'next/navigation'
+import { useLayoutEffect } from 'react'
+import { toast } from 'sonner'
 
 export default function useScreenSizeWarningToast() {
-  useEffect(() => {
-    if (window.innerWidth <= 768 && !sessionStorage.getItem('toastShown')) {
-      toast({
-        title: 'Not optimized for mobile devices!',
-        description:
-          "The editor is not optimized for mobile devices, hence many feature won't work as expected. Please use a desktop device for the best experience.",
-      })
-      sessionStorage.setItem('toastShown', 'true')
+  const router = useRouter()
+  useLayoutEffect(() => {
+    if (window.innerWidth <= 768) {
+      // TODO: consider what's needed or not
+      // blur the entire document
+      // document.body.style.filter = 'blur(5px)'
+      router.push('/') // TODO: maybe don't push to home page?
+      // document.body.style.filter = 'none'
+      // if (sessionStorage.getItem('toastShown')) return
+      toast.warning("Not optimized for mobile devices!", { description: "The editor is not optimized for mobile devices, hence many feature won't work as expected. Please use a desktop device for the best experience." })
+      // sessionStorage.setItem('toastShown', 'true')
     }
   }, [])
 }
