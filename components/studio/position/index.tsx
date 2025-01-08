deb68b4bf8201340aba5c6b84fb47ffd186e91af
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-08 18:11:04 -0500

    ✨ format position options

--- components/studio/position/index.tsx
+++ components/studio/position/index.tsx
@@ -8,17 +8,14 @@ export default function PositionOptions() {
     return (
         <>
             <div className="mb-8">
-                <div className="mb-4 mt-4 flex items-center px-1 md:max-w-full">
-                    <h1 className="text-[0.85rem]">Position:</h1>
+                <div className="mb-4 flex items-center px-1 md:max-w-full">
+                    <h1 className="text-[0.85rem]">Position</h1>
                 </div>
-
                 <PositionControl />
             </div>
-
             <div className="mb-2">
                 <SizeOption text="Scale" />
             </div>
-
             <div className="mb-4">
                 <TranslateOption />
             </div>
