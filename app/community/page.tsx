22914a5af648e1ecb1d9b5e6a62d6bcfb732646c
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-10 12:24:19 -0500

    📍 add open graph / seo metadata to community page

--- app/community/page.tsx
+++ app/community/page.tsx
@@ -1,6 +1,53 @@
-import { prisma } from "@/lib/prisma";
 import { auth } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { Metadata } from "next";
 import { ImageGallery } from "./image-gallery";
+
+export const metadata: Metadata = {
+    generator: 'Next.js Baby!',
+    applicationName: 'Seleneo',
+    title: 'Community Showcase - Seleneo',
+    description: 'Explore the community-made designs and images, as shared by our users.',
+    robots: 'index, follow',
+    referrer: 'origin-when-cross-origin',
+    keywords: ['design', 'tooling', 'open-source', 'digital assets', 'social media', 'images',
+        'thumbnails', 'screenshots', 'free design tooling', 'seleneo', 'seleneo design tooling',
+        'graphics', 'editing', 'creative', 'visual', 'marketing', 'branding', 'digital tools',
+        'image editor', 'social media tools', 'content creation', 'web design', 'UI/UX', 'thumbnail generator',
+        'design software', 'media creation', 'asset management', 'social graphics', 'visual content',
+        'design platform', 'digital marketing', 'image optimization', 'design resources', 'content tools',
+        'digital design', 'online editor', 'web tools', 'creative suite', 'design automation',
+        'social assets', 'media toolkit'],
+    authors: [{ name: 'Nyumat', url: 'https://github.com/nyumat' }],
+    creator: 'Nyumat',
+    publisher: 'Nyumat',
+    formatDetection: {
+        email: false,
+        address: false,
+        telephone: false,
+    },
+    metadataBase: new URL('https://freedesign.fyi'),
+    alternates: {
+        canonical: '/',
+    },
+    openGraph: {
+        title: 'Community Showcase - Seleneo',
+        description: 'Explore the community-made designs and images, as shared by our users.',
+        url: 'https://freedesign.fyi',
+        siteName: 'Seleneo',
+        images: [
+            {
+                url: 'https://freedesign.fyi/api/og?title=Community',
+                width: 1800,
+                height: 1600,
+                alt: 'Seleneo Community Gallery',
+            },
+        ],
+        locale: 'en_US',
+        type: 'website',
+    },
+}
+
 
 async function getAllPublicImages() {
     const images = await prisma.userImage.findMany({
@@ -42,7 +89,7 @@ export default async function CommunityPage() {
             <div className="max-w-[100vw] p-4">
                 <ImageGallery
                     images={images}
-                    currentUserId={currentUser?.user?.id}
+                    currentUserId={currentUser?.user?.name || ""}
                     title="Community Gallery"
                     description={`Explore ${images.length} images shared by our community`}
                 />
