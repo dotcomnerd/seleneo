b2f72bbcdb974c2780a99ebfd372e3444647045a
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-09 21:19:09 -0500

    🚀 add session callback for user id on client

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,4 +6,10 @@ import GitHub from "next-auth/providers/github"
 export const { handlers, signIn, signOut, auth } = NextAuth({
     providers: [GitHub],
     adapter: PrismaAdapter(prisma),
+    callbacks: {
+        async session({ session, user }) {
+            session.user.id = user.id
+            return session
+        },
+    },
 })
