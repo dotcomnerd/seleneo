a2ede7dd0c6f21649308719aa4939d3b5598163e
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-11 19:40:11 -0500

    🚀 lower cta line opacity and prominence

--- components/marketing/call-to-action.tsx
+++ components/marketing/call-to-action.tsx
@@ -44,10 +44,10 @@ export function CallToAction() {
                                     </Button>
 
                                     <div className="w-[40rem] h-40 relative mt-4">
-                                        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
-                                        <div className="absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
-                                        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
-                                        <div className="absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
+                                        <div className="opacity-30 absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-[2px] w-3/4 blur-sm" />
+                                        <div className="opacity-30 absolute inset-x-20 top-0 bg-gradient-to-r from-transparent via-indigo-500 to-transparent h-px w-3/4" />
+                                        <div className="opacity-30 absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-[5px] w-1/4 blur-sm" />
+                                        <div className="opacity-30 absolute inset-x-60 top-0 bg-gradient-to-r from-transparent via-sky-500 to-transparent h-px w-1/4" />
                                         <SparklesCore
                                             background="transparent"
                                             minSize={0.4}
