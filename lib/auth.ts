147d5dc87f0ce110f4442fd312aaf318919facd7
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-11 19:12:16 -0500

    🐛 use actual username for github oauth

--- lib/auth.ts
+++ lib/auth.ts
@@ -4,12 +4,37 @@ import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [GitHub],
+    providers: [GitHub({
+        profile(profile) {
+            return {
+                id: profile.id.toString(),
+                name: profile.login,
+                email: profile.email,
+                image: profile.avatar_url
+            }
+        }
+    })],
     adapter: PrismaAdapter(prisma),
     callbacks: {
         async session({ session, user }) {
             session.user.id = user.id
             return session
+        },
+        async signIn({ user, profile }) {
+            if (profile && 'login' in profile) {
+                const dbUser = await prisma.user.findUnique({
+                    where: { id: user.id },
+                    select: { name: true }
+                })
+
+                if (dbUser?.name?.includes(' ')) {
+                    await prisma.user.update({
+                        where: { id: user.id },
+                        data: { name: profile.login as string }
+                    })
+                }
+            }
+            return true
         },
     },
 })
