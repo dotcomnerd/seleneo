ba8624c2b7e2b99b2f1de10bf32ddb7981ac6b63
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-13 18:30:44 -0500

    📦 initial extended support for more tiptap extensions

--- hooks/use-editor.ts
+++ hooks/use-editor.ts
@@ -2,10 +2,15 @@ import { useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 import { Color } from '@tiptap/extension-color'
 import TextStyle from '@tiptap/extension-text-style'
+import FontFamily from '@tiptap/extension-font-family'
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import TextAlign from '@tiptap/extension-text-align'
 
 export default function useTiptapEditor() {
   const editor = useEditor({
-    extensions: [StarterKit, Color, TextStyle],
+      extensions: [StarterKit, Color, TextStyle, FontFamily, Document, Paragraph, Text, TextStyle, TextAlign],
     content: 'Double click to edit',
   })
 
