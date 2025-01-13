75076253a74b3697a43e61c2e3853a18cbc3c33f
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-13 18:40:14 -0500

    🙅🏿‍♂️ remove overbearing shadcn focus ring on btns

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,7 +6,7 @@ import { forwardRef } from 'react'
 import { Slot } from '@radix-ui/react-slot'
 
 const buttonVariants = cva(
-    'inline-flex items-center justify-center rounded-lg text-sm font-medium ring-offset-background transition-colors whitespace-nowrap focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 focus:z-10',
+    'inline-flex items-center justify-center rounded-lg text-sm font-medium ring-offset-background transition-colors whitespace-nowrap disabled:pointer-events-none disabled:opacity-50 focus:z-10',
     {
         variants: {
             variant: {
