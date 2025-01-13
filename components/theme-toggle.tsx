19ced056d5b38bdf1bce890f9f0a539a03183af5
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-13 18:13:43 -0500

    🎨 tokenize theme toggle

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -10,6 +10,7 @@ import {
     DropdownMenuItem,
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { cn } from '@/lib/utils'
 
 export function ThemeToggle() {
     const { setTheme } = useTheme()
@@ -17,9 +18,9 @@ export function ThemeToggle() {
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
-                <Button variant="ghost" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <Button className={cn('hover:bg-card opacity-80 hover:opacity-100 bg-card outline-none ring-0 stroke-none border-none')} size="icon">
+                    <Sun className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 light:rotate-0 light:scale-100 stroke-black dark:stroke-primary" />
+                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 light:-rotate-90 light:scale-0 stroke-black dark:stroke-primary" />
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
