dca07e242d79b3b939db27508d439c9c209ad6d1
dotcomnerd <Tomxxxfresh@gmail.com>
2025-01-13 19:32:07 -0500

    🐳 fmt use-tip-tap hook

--- hooks/use-editor.ts
+++ hooks/use-editor.ts
@@ -1,18 +1,18 @@
-import { useEditor } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
 import { Color } from '@tiptap/extension-color'
-import TextStyle from '@tiptap/extension-text-style'
-import FontFamily from '@tiptap/extension-font-family'
 import Document from '@tiptap/extension-document'
+import FontFamily from '@tiptap/extension-font-family'
 import Paragraph from '@tiptap/extension-paragraph'
 import Text from '@tiptap/extension-text'
 import TextAlign from '@tiptap/extension-text-align'
+import TextStyle from '@tiptap/extension-text-style'
+import { useEditor } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
 
 export default function useTiptapEditor() {
-  const editor = useEditor({
-      extensions: [StarterKit, Color, TextStyle, FontFamily, Document, Paragraph, Text, TextStyle, TextAlign],
-    content: 'Double click to edit',
-  })
+    const editor = useEditor({
+        extensions: [StarterKit, Color, TextStyle, FontFamily, Document, Paragraph, Text, TextStyle, TextAlign],
+        content: 'Double click to edit',
+    })
 
-  return { editor }
+    return { editor }
 }
