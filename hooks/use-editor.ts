02158bfb40c59fd5513d1bdb8328985c7353afd3
dotcomnerd <Tomxxxfresh@gmail.com>
2025-09-03 23:20:51 -0700

    🐛 fix usage of tip-tap `useEditor` (thanks vansh)

--- hooks/use-editor.ts
+++ hooks/use-editor.ts
@@ -1,19 +1,31 @@
 import { Color } from '@tiptap/extension-color'
-import Document from '@tiptap/extension-document'
 import FontFamily from '@tiptap/extension-font-family'
-import Paragraph from '@tiptap/extension-paragraph'
-import Text from '@tiptap/extension-text'
 import TextAlign from '@tiptap/extension-text-align'
 import TextStyle from '@tiptap/extension-text-style'
 import { useEditor } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
 
-
-
 export default function useTiptapEditor(initialContent: string = '') {
     const editor = useEditor({
-        extensions: [StarterKit, Color, TextStyle, FontFamily, Document, Paragraph, Text, TextStyle, TextAlign],
+        extensions: [
+            StarterKit,
+            TextStyle,
+            Color.configure({
+                types: ['textStyle'],
+            }),
+            FontFamily.configure({
+                types: ['textStyle'],
+            }),
+            TextAlign.configure({
+                types: ['heading', 'paragraph'],
+                defaultAlignment: 'center',
+            }),
+        ],
         content: initialContent,
+        onCreate: ({ editor }) => {
+            editor.commands.selectAll()
+            editor.commands.setTextAlign('center')
+        }
     })
 
     return { editor }
